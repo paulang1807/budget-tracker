2704c1d5d67e1d8f5ac8ef61743103cfa69453af
Angshuman Paul <angshuman_paul2@intuit.com>
2021-08-02 21:40:24 -0700

    Add transactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
+    const { addTransaction } = useContext(GlobalContext);
     const [text, setText] = useState("")
     const [amount, setAmount] = useState(0)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const newTrans = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+        addTransaction(newTrans)
+    }
 
     return (
         <>
             <h3>Add new transaction</h3>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(event) => setText(event.target.value)} placeholder="Enter text..." />
